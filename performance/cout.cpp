f62677f97bc86fc6141db54b1eed215a3051a2fc
Shinwoo Park <natural7530@gmail.com>
2019-05-26 01:09:48 +0900

    Add cout.cpp

---
+++ performance/cout.cpp
@@ -0,0 +1,99 @@
+/*
+    Shinwoo Park
+    natural7530@gmail.com
+
+    CC0 1.0 Universal
+*/
+
+#include <chrono>
+#include <iostream>
+
+#include <cstdio>
+
+using namespace std;
+using namespace std::chrono;
+
+duration<double> t[4];
+
+int main()
+{
+	/*
+	If you set ios_base::sync_with_stdio(false),
+	std::cout is fast enough.
+	*/
+
+	/* cout */
+	cout.flush();
+	auto st = high_resolution_clock::now();
+	for (int i = 0; i < 1e4; ++i) {
+		cout << "abcdefghijklnmopqrstuvwxyz" << endl;
+	}
+
+	auto ed = high_resolution_clock::now();
+	t[0] = ed - st;
+
+	/* cout optimized */
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout.flush();
+	st = high_resolution_clock::now();
+	for (int i = 0; i < 1e4; ++i) {
+		cout << "abcdefghijklnmopqrstuvwxyz" << endl;
+	}
+
+	ed = high_resolution_clock::now();
+	t[1] = ed - st;
+
+	// prepare for c functions
+	ios_base::sync_with_stdio(true);
+
+	/* printf */
+	fflush(stdout);
+	st = high_resolution_clock::now();
+	for (int i = 0; i < 1e4; ++i) {
+		printf("abcdefghijklnmopqrstuvwxyz\n");
+	}
+
+	ed = high_resolution_clock::now();
+	t[2] = ed - st;
+
+	/* puts */
+	fflush(stdout);
+	st = high_resolution_clock::now();
+	for (int i = 0; i < 1e4; ++i) {
+		puts("abcdefghijklnmopqrstuvwxyz");
+	}
+
+	ed = high_resolution_clock::now();
+	t[3] = ed - st;
+
+	/* print all results */
+
+	// Optimization
+	ios_base::sync_with_stdio(true);
+
+#if _WIN32
+	system("cls");
+#else
+	system("clear; clear");
+#endif // _WIN32
+
+	cout.precision(2);
+	cout.setf(ios::fixed);
+
+	cout.flush();
+
+	cout << endl
+	     << "cout:\t\t\t" << t[0].count() << endl
+	     << "cout optimized:\t\t" << t[1].count() << endl
+	     << "printf:\t\t\t" << t[2].count() << endl
+	     << "puts:\t\t\t" << t[3].count() << endl
+	     << endl
+	     << "cout/printf:\t\t" << t[0].count() / t[2].count() << endl
+	     << "cout optimized/printf:\t" << t[1].count() / t[2].count() << endl
+	     << "puts/printf:\t\t" << t[2].count() / t[3].count() << endl;
+
+	return 0;
+}
