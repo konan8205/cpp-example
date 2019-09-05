e2c9a5ca5f70c9fb235d0496c4b6304267fe86c0
Shinwoo Park <natural7530@gmail.com>
2019-09-05 16:51:00 +0900

    Fix incorrect behavior of static-libc++.py

--- compile/clang/static-libc++.py
+++ compile/clang/static-libc++.py
@@ -19,10 +19,19 @@ libcpp_include = "/usr/lib/llvm-" + llvm_version + "/include/c++/v1"
 libcpp_lib = "/usr/lib/llvm-" + llvm_version + "/lib/libc++.a"
 libcppabi_lib = "/usr/lib/llvm-" + llvm_version + "/lib/libc++abi.a"
 
-clang_argv = ['clang++-' + llvm_version]
+clang_argv = [
+    'clang++-' + llvm_version,
+    '-nostdinc++',
+    '-nostdlib++',
+    '-I' + libcpp_include
+]
+
 for idx in range(2, len(sys.argv)):
-    print(sys.argv[idx])
     clang_argv.append(sys.argv[idx])
+
+clang_argv.append(libcpp_lib)
+clang_argv.append(libcppabi_lib)
+clang_argv.append('-pthread')
 
 proc = subprocess.Popen(clang_argv)
 proc.wait()
