6296837a9239137d074cfc464682abdeed2198d5
Shinwoo Park <natural7530@gmail.com>
2019-12-12 03:26:49 +0900

    Update asm/cpuid.cpp

--- asm/cpuid.cpp
+++ asm/cpuid.cpp
@@ -6,18 +6,20 @@
 */
 
 #include <cstdint>
+#include <cstring>
 #include <iostream>
+#include <string>
 
 #ifdef _WIN32
 #include <intrin.h>
 #endif
 
-struct cpuid_status_s {
+struct cpuid_s {
     uint32_t eax, ebx, ecx, edx;
 };
 
-cpuid_status_s get_cpuid(uint32_t eax, uint32_t ecx = 0) {
-    cpuid_status_s cpuInfo;
+cpuid_s get_cpuid(uint32_t eax, uint32_t ecx = 0) {
+    cpuid_s cpuInfo;
 
 #ifdef _WIN32
     __cpuidex((int *)&cpuInfo, eax, ecx);
@@ -31,18 +33,42 @@ cpuid_status_s get_cpuid(uint32_t eax, uint32_t ecx = 0) {
     return cpuInfo;
 }
 
+std::string to_bool(bool val) {
+    return val == true ? "true" : "false";
+}
+
 int main() {
     // https://en.wikipedia.org/wiki/CPUID#Calling_CPUID
 
-    cpuid_status_s cpuid1 = get_cpuid(1, 0);
+    cpuid_s cpuid0 = get_cpuid(0), cpuid1 = get_cpuid(1),
+            cpuid70 = get_cpuid(7, 0), cpuid71 = get_cpuid(7, 1);
 
-    bool htt = cpuid1.edx & (1 << 28);
-    bool sse2 = cpuid1.edx & (1 << 19);
-    bool avx2 = get_cpuid(7, 0).ebx & (1 << 5);
+    // Highest Function Parameter
+    uint32_t hfp = cpuid0.eax;
 
-    std::cout << "Hyper-threading: " << htt << std::endl
-              << "SSE2 support:    " << sse2 << std::endl
-              << "AVX2 support:    " << avx2 << std::endl;
+    // Manufacturer ID
+    char id[13];
+    memcpy(id, &cpuid0.ebx, 4);
+    memcpy(id + 4, &cpuid0.edx, 4);
+    memcpy(id + 8, &cpuid0.ecx, 4);
+    id[12] = 0;
+
+    std::cout
+        << "Highest Function Parameter: 0x" << std::hex << std::uppercase << hfp
+        << "\nManufacturer:               " << id
+        << "\nHyper-threading:            " << to_bool(cpuid1.edx & (1 << 28))
+        << "\nSSE support:                " << to_bool(cpuid1.edx & (1 << 25))
+        << "\nSSE2 support:               " << to_bool(cpuid1.edx & (1 << 26))
+        << "\nSSE3 support:               " << to_bool(cpuid1.ecx & (1 << 0))
+        << "\nSSSE3 support:              " << to_bool(cpuid1.ecx & (1 << 9))
+        << "\nSSE4a support:              " << to_bool(cpuid71.ecx & (1 << 6))
+        << "\nSSE4.1 support:             " << to_bool(cpuid1.ecx & (1 << 19))
+        << "\nSSE4.2 support:             " << to_bool(cpuid1.ecx & (1 << 20))
+        << "\nMMX support:                " << to_bool(cpuid1.edx & (1 << 23))
+        << "\nExtended MMX support:       " << to_bool(cpuid71.edx & (1 << 22))
+        << "\nAVX support:                " << to_bool(cpuid1.ecx & (1 << 28))
+        << "\nAVX2 support:               " << to_bool(cpuid70.ebx & (1 << 5))
+        << std::endl;
 
     return 0;
 }
