2f4a1e5d4f8b6a8f44778f054352b09bc140b999
Shinwoo Park <natural7530@gmail.com>
2020-01-03 21:38:07 +0900

    Fix incorrect behavior of asm/cpuid.cpp

--- asm/cpuid.cpp
+++ asm/cpuid.cpp
@@ -7,6 +7,7 @@
 
 #include <cstdint>
 #include <cstring>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
@@ -41,7 +42,8 @@ int main() {
     // https://en.wikipedia.org/wiki/CPUID#Calling_CPUID
 
     cpuid_s cpuid0 = get_cpuid(0), cpuid1 = get_cpuid(1),
-            cpuid70 = get_cpuid(7, 0), cpuid71 = get_cpuid(7, 1);
+            cpuid7_0 = get_cpuid(7, 0), cpuid80 = get_cpuid(0x80000000),
+            cpuid81 = get_cpuid(0x80000001);
 
     // Highest Function Parameter
     uint32_t hfp = cpuid0.eax;
@@ -53,22 +55,26 @@ int main() {
     memcpy(id + 8, &cpuid0.ecx, 4);
     id[12] = 0;
 
+#define format_addr \
+    std::hex << std::uppercase << std::setfill('0') << std::setw(2)
+
     std::cout
-        << "Highest Function Parameter: 0x" << std::hex << std::uppercase << hfp
-        << "\nManufacturer:               " << id
-        << "\nHyper-threading:            " << to_bool(cpuid1.edx & (1 << 28))
-        << "\nSSE support:                " << to_bool(cpuid1.edx & (1 << 25))
-        << "\nSSE2 support:               " << to_bool(cpuid1.edx & (1 << 26))
-        << "\nSSE3 support:               " << to_bool(cpuid1.ecx & (1 << 0))
-        << "\nSSSE3 support:              " << to_bool(cpuid1.ecx & (1 << 9))
-        << "\nSSE4a support:              " << to_bool(cpuid71.ecx & (1 << 6))
-        << "\nSSE4.1 support:             " << to_bool(cpuid1.ecx & (1 << 19))
-        << "\nSSE4.2 support:             " << to_bool(cpuid1.ecx & (1 << 20))
-        << "\nMMX support:                " << to_bool(cpuid1.edx & (1 << 23))
-        << "\nExtended MMX support:       " << to_bool(cpuid71.edx & (1 << 22))
-        << "\nAVX support:                " << to_bool(cpuid1.ecx & (1 << 28))
-        << "\nAVX2 support:               " << to_bool(cpuid70.ebx & (1 << 5))
-        << std::endl;
+        << "Highest Function Parameter:  0x" << format_addr << hfp
+        << "\nHighest Extended Function:   0x" << cpuid80.eax
+        << "\nManufacturer:                " << id
+        << "\nHyper-threading:             " << to_bool(cpuid1.edx & (1 << 28))
+        << "\nSSE support:                 " << to_bool(cpuid1.edx & (1 << 25))
+        << "\nSSE2 support:                " << to_bool(cpuid1.edx & (1 << 26))
+        << "\nSSE3 support:                " << to_bool(cpuid1.ecx & (1 << 0))
+        << "\nSSSE3 support:               " << to_bool(cpuid1.ecx & (1 << 9))
+        << "\nSSE4a support:               " << to_bool(cpuid81.ecx & (1 << 6))
+        << "\nSSE4.1 support:              " << to_bool(cpuid1.ecx & (1 << 19))
+        << "\nSSE4.2 support:              " << to_bool(cpuid1.ecx & (1 << 20))
+        << "\nMMX support:                 " << to_bool(cpuid1.edx & (1 << 23))
+        << "\nExtended MMX support:        " << to_bool(cpuid81.edx & (1 << 22))
+        << "\nAVX support:                 " << to_bool(cpuid1.ecx & (1 << 28))
+        << "\nAVX2 support:                " << to_bool(cpuid7_0.ebx & (1 << 5))
+        << "\n";
 
     return 0;
 }
